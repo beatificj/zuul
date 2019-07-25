44deeea0bfd9bbdbbf57a6b7197eaae23cf3154e
beatific <beatific@sk.com>
2019-07-25 09:29:18 +0900

    oops

--- oops/src/main/java/com/skcc/oops/OopsApplication.java
+++ oops/src/main/java/com/skcc/oops/OopsApplication.java
@@ -2,7 +2,8 @@ package com.skcc.oops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import com.skcc.oops.kubernetes.EnableOopsClients;
 
 
 @EnableOopsClients
