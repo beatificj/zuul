91a756045d989b12c92b176d8f9e83c707b6e370
beatific <beatific@sk.com>
2019-07-31 11:26:58 +0900

    oops

--- oops/src/main/java/com/skcc/oops/BooksController.java
+++ oops/src/main/java/com/skcc/oops/BooksController.java
@@ -3,7 +3,7 @@ package com.skcc.oops;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name="books", url="http://books:8080")
+@FeignClient(name="books")
 public interface BooksController {
 	
 	@RequestMapping("/harry")
