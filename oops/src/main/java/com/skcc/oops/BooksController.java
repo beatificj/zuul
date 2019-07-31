1100ae3f9a2ec93682960e9f9c00769eab10d7b7
beatific <beatific@sk.com>
2019-07-31 11:09:53 +0900

    istio url

--- oops/src/main/java/com/skcc/oops/BooksController.java
+++ oops/src/main/java/com/skcc/oops/BooksController.java
@@ -3,7 +3,7 @@ package com.skcc.oops;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name="books")
+@FeignClient(name="books", url="http://books:8080")
 public interface BooksController {
 	
 	@RequestMapping("/harry")
