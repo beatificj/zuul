8f48e1e716c78fc412fe23d6371a196e00b44502
beatific <beatific@sk.com>
2019-09-10 13:11:45 +0900

    spelling edited

--- auth-demo/src/main/java/com/sktelecom/auth/demo/AuthController.java
+++ auth-demo/src/main/java/com/sktelecom/auth/demo/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-	@RequestMapping("/sucess")
+	@RequestMapping("/success")
 	public String auth() {
 		return "success";
 	}
